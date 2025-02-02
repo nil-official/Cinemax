e93198b0af60b9c8d690afbb345fac69ddc95a28
nil-official <niladrichakraborty16@gmail.com>
2025-02-03 00:29:12 +0530

    Showtime & Seat Page Impl

--- frontend/src/components/Showtimes/ShowtimesCard.jsx
+++ frontend/src/components/Showtimes/ShowtimesCard.jsx
@@ -1,58 +1,69 @@
-import React from "react";
-import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import { Box, Button, Card, CardContent, Typography, useMediaQuery } from "@mui/material";
 import { FaMobileAlt } from "react-icons/fa";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { selectShowtime } from "../../store/actions/showtimes";
 
-const ShowtimesCard = ({ showtimes }) => {
+const ShowtimesCard = ({ category, showtimes, movieId, showtime }) => {
 
-    const handleShowtimeClick = () => {
-        console.log("Showtime Clicked");
-    };
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const isMobile = useMediaQuery('(max-width:600px)');
+
+    // const handleShowtimeClick = () => {
+    //     console.log("Showtime Clicked");
+    //     navigate(`/movie/${movieId}/showtimes/${showtime._id}/seatlayout`);
+    // };
+    useEffect(() => {
+        if (showtime) {
+            navigate(`/movie/${movieId}/showtimes/${showtime._id}/seatlayout`);
+        }
+    }, [showtime, navigate, movieId]);
+
+    const handleShowtimeSelect = (show) => {
+        dispatch(selectShowtime(show));
+    }
 
     return (
-        <Card
-            sx={{
-                mb: 3,
-                borderRadius: 2,
-                boxShadow: 3,
-            }}
-        >
+        <Card sx={{ mb: 3, borderRadius: 2, boxShadow: 3 }}>
             <CardContent>
-                <Box
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                    p={2}
-                >
+                <Box sx={{
+                    p: isMobile ? 1 : 2,
+                    display: isMobile ? 'block' : 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }}>
                     {/* Left Section: Theatre & Pricing */}
-                    <Box>
-                        {/* <Typography variant="h6" fontWeight="bold">
-                            {showtimes._id}
-                        </Typography> */}
+                    <Box sx={{ mb: isMobile ? 2 : 0 }}>
                         <Typography variant="h6" fontWeight="bold">
-                            {showtimes.screenId.name}
+                            {category}
                         </Typography>
                         <Box display="flex" alignItems="center" gap={1}>
                             <Typography variant="body2" color="primary" display="flex" alignItems="center">
                                 <FaMobileAlt style={{ marginRight: '5px' }} />
                                 M-Ticket
                             </Typography>
-                            <Typography variant="body1" color="textSecondary">
-                                ₹{showtimes.screenId.price} onwards
+                            <Typography variant="body1">
+                                ₹{showtimes[0].screenId.price} onwards
                             </Typography>
                         </Box>
                     </Box>
 
                     {/* Right Section: Showtimes */}
-                    <Box display="flex" alignItems="center" gap={1}>
-                        <Button
-                            variant="outlined"
-                            fontSize="large"
-                            sx={{ fontWeight: 'bold', fontSize: '16px' }}
-                            onClick={handleShowtimeClick}
-                        >
-                            {format(new Date(showtimes.startAt), 'hh:mm a')}
-                        </Button>
+                    <Box display="flex" alignItems="center" gap={1} flexWrap="wrap">
+                        {showtimes.map((show) => (
+                            <Button
+                                key={show._id}
+                                variant="outlined"
+                                fontSize="large"
+                                sx={{ fontWeight: 'bold', fontSize: '16px' }}
+                                onClick={() => handleShowtimeSelect(show)}
+                            >
+                                {format(new Date(show.startAt), 'hh:mm a')}
+                            </Button>
+                        ))}
                     </Box>
                 </Box>
             </CardContent>
--- frontend/src/pages/SeatPage.jsx
+++ frontend/src/pages/SeatPage.jsx
@@ -1,11 +1,55 @@
-import React, { useState } from "react";
-import { Box, Typography, Button, Grid, Grid2, Select, MenuItem } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Button, Grid2, Select, MenuItem, Skeleton, useMediaQuery, useTheme, Slide } from "@mui/material";
 import { seatData } from "../../data/screen1";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-const SeatLayoutPage = () => {
+const SeatPage = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { movieId, showtimeId } = useParams();
+    const { screen, status, error } = useSelector((state) => state.screenState);
+    const { selectedShowtime } = useSelector((state) => state.showtimeState);
 
-    const [selectedSeats, setSelectedSeats] = useState([]); // Track selected seats
-    const [seatLimit, setSeatLimit] = useState(1); // User-selected seat limit
+    const [selectedSeats, setSelectedSeats] = useState([]);
+    const [seatLimit, setSeatLimit] = useState(1);
+    const [loading, setLoading] = useState(true);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [showPayButton, setShowPayButton] = useState(false);
+
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+    // useEffect(() => {
+    //     if (status === 'idle') {
+    //         dispatch(fetchScreen(selectedShowtime.screenId));
+    //     }
+    // }, [status, dispatch]);
+
+    // Simulate loading
+    useEffect(() => {
+        const timer = setTimeout(() => setLoading(false), 1000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    // Handle show pay now button
+    useEffect(() => {
+        let totalCost = 0;
+        selectedSeats.forEach(({ row, seats }) => {
+            seatData.layout.forEach(category => {
+                if (category.rows.some(r => r.row === row)) {
+                    totalCost += seats.length * category.price;
+                }
+            });
+        });
+        setTotalPrice(totalCost);
+        setShowPayButton(countSelectedSeats(selectedSeats) == seatLimit);
+    }, [selectedSeats]);
+
+    // Count selected seats
+    const countSelectedSeats = (selectedSeats) => {
+        return selectedSeats.reduce((total, { seats }) => total + seats.length, 0);
+    };
 
     // Check if seat is already booked
     const isSeatBooked = (row, seat) => {
@@ -37,42 +81,79 @@ const SeatLayoutPage = () => {
         });
     };
 
+    // console.log(selectedSeats);
+
     // Handle seat limit change
     const handleSeatLimitChange = (e) => {
         setSeatLimit(e.target.value);
         setSelectedSeats([]);
     };
 
-    console.log(selectedSeats);
+    // Handle payment
+    const handlePayment = () => {
+        console.log("Payment Initiated");
+    }
 
     return (
-        <Box sx={{ p: 3, minHeight: "100vh", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{ p: 2, pb: 10, minHeight: "100vh", display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Box sx={{ width: '100%', mb: 2 }}>
                 <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h4" gutterBottom>
+                    <Typography variant={isMobile ? "h5" : "h4"} sx={{ fontWeight: 600 }} gutterBottom>
                         Seat Layout
                     </Typography>
-                    <Select value={seatLimit} sx={{ minWidth: 70 }} onChange={handleSeatLimitChange}>
+                    <Select value={seatLimit} sx={{ minWidth: { xs: 50, sm: 70 } }} onChange={handleSeatLimitChange}>
                         {[...Array(5)].map((_, i) => (
                             <MenuItem key={i + 1} value={i + 1}>{i + 1}</MenuItem>
                         ))}
                     </Select>
                 </Box>
             </Box>
-            {seatData.layout.map((section, index) => (
-                <Box key={index} sx={{ mb: 2, width: { xs: '100%', sm: 'auto' }, overflowX: 'auto', flexWrap: 'nowrap' }}>
-                    <Typography variant="h6" sx={{ mb: 2 }}>
-                        {section.category}
-                    </Typography>
-                    {section.rows.map((rows, rowIndex) => (
-                        <Grid2 key={rowIndex} container alignItems="center" sx={{ mb: 1, flexWrap: 'nowrap' }}>
-                            <Box sx={{ width: 24, mr: { xs: 1, sm: 0 } }}>
-                                <Typography variant="body1" sx={{ width: 24, mr: { xs: 1, sm: 0 } }}>
-                                    {rows.row}
-                                </Typography>
-                            </Box>
-                            {
-                                rows.seats.map((seat, seatIndex) => (
+            {loading ? (
+                seatData.layout.map((section, index) => (
+                    <Box key={index} sx={{
+                        mb: 2,
+                        width: { xs: '100%', sm: 'auto' },
+                        overflowX: 'auto',
+                        flexWrap: 'nowrap',
+                    }}>
+                        <Typography variant="h6" sx={{ mb: 2 }}>
+                            <Skeleton width={200} height={30} />
+                        </Typography>
+                        {section.rows.map((rows, rowIndex) => (
+                            <Grid2 key={rowIndex} container alignItems="center" sx={{ mb: 1, flexWrap: 'nowrap' }}>
+                                <Box sx={{ width: 24, mr: 2 }}>
+                                    <Skeleton variant="rectangular" sx={{ width: 24, height: 24 }} />
+                                </Box>
+                                {rows.seats.map((seat, seatIndex) => (
+                                    seat === null ? (
+                                        <Box key={seatIndex} sx={{ width: 24, mr: { xs: 2, sm: 0 } }} />
+                                    ) : (
+                                        <Skeleton key={seatIndex} variant="rectangular" sx={{ minWidth: 32, minHeight: 32, margin: '5px', borderRadius: '5px' }} />
+                                    )
+                                ))}
+                            </Grid2>
+                        ))}
+                    </Box>
+                ))
+            ) : (
+                seatData.layout.map((section, index) => (
+                    <Box key={index} sx={{
+                        mb: 2,
+                        width: { xs: '100%', sm: 'auto' },
+                        overflowX: 'auto',
+                        flexWrap: 'nowrap',
+                    }}>
+                        <Typography variant="h6" sx={{ mb: 2 }}>
+                            {section.category}
+                        </Typography>
+                        {section.rows.map((rows, rowIndex) => (
+                            <Grid2 key={rowIndex} container alignItems="center" sx={{ mb: 1, flexWrap: 'nowrap' }}>
+                                <Box sx={{ width: 24, mr: { xs: 1, sm: 0 } }}>
+                                    <Typography variant="body1" sx={{ width: 24, mr: { xs: 1, sm: 0 } }}>
+                                        {rows.row}
+                                    </Typography>
+                                </Box>
+                                {rows.seats.map((seat, seatIndex) => (
                                     seat === null ? (
                                         <Box key={seatIndex} sx={{ width: 24, mr: { xs: 2, sm: 0 } }} />
                                     ) : (
@@ -91,15 +172,38 @@ const SeatLayoutPage = () => {
                                             {seat}
                                         </Button>
                                     )
-                                ))
-                            }
-                        </Grid2>
-                    ))}
+                                ))}
+                            </Grid2>
+                        ))}
+                    </Box>
+                ))
+            )}
+
+            {/* Pay Button with Sliding Animation */}
+            <Slide direction="up" in={showPayButton} mountOnEnter unmountOnExit>
+                <Box sx={{
+                    position: 'fixed',
+                    bottom: 0,
+                    left: 0,
+                    height: `${isMobile ? '80px' : '80px'}`,
+                    width: '100%',
+                    zIndex: '500',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: `${theme.palette.background.default}`,
+                    boxShadow: `0px 2px 15px 15px ${theme.palette.background.default}`
+                }} >
+                    <Button variant="outlined"
+                        sx={{ minWidth: isMobile ? 300 : 200, fontSize: "16px", borderRadius: "8px", color: "white", py: 1 }}
+                        onClick={handlePayment}
+                    >
+                        Pay Rs.{totalPrice}
+                    </Button>
                 </Box>
-            ))
-            }
+            </Slide >
         </Box >
     );
 };
 
-export default SeatLayoutPage;
+export default SeatPage;
